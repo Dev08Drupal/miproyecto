947353365380bff40bd5a9fda0b86df608908673
Diego Ricaurte <dricaurte@micrositios.net>
2024-09-11 18:58:57 -0500

    Agregando header y footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,86 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer>
-      <p>&copy; 2024 Your Website. All rights reserved.</p>
+    <footer className="text-center text-lg-start bg-light">
+      <div className="container pt-4 pb-0">
+        <section className="">
+          <div className="row">
+            <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+              <h6 className="text-uppercase mb-4 font-weight-bold">Company name</h6>
+              <p>
+                Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet, consectetur
+                adipisicing elit.
+              </p>
+            </div>
+
+            <hr className="w-100 clearfix d-md-none" />
+
+            <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+              <h6 className="text-uppercase mb-4 font-weight-bold">Products</h6>
+              <p>
+                <NavLink to="/actividad1">Actividad 1</NavLink>
+              </p>
+              <p>
+                <NavLink to="/actividad1">Actividad 1</NavLink>
+              </p>
+              <p>
+                <NavLink to="/actividad1">Actividad 1</NavLink>
+              </p>
+              <p>
+                <NavLink to="/actividad1">Actividad 1</NavLink>
+              </p>
+            </div>
+
+            <hr className="w-100 clearfix d-md-none" />
+
+            <hr className="w-100 clearfix d-md-none" />
+
+            <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+              <h6 className="text-uppercase mb-4 font-weight-bold">Contact</h6>
+              <p>
+                <i className="fas fa-home mr-3"></i> New York, NY 10012, US
+              </p>
+              <p>
+                <i className="fas fa-envelope mr-3"></i> info@gmail.com
+              </p>
+              <p>
+                <i className="fas fa-phone mr-3"></i> + 01 234 567 88
+              </p>
+              <p>
+                <i className="fas fa-print mr-3"></i> + 01 234 567 89
+              </p>
+            </div>
+            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+              <h6 className="text-uppercase mb-4 font-weight-bold">Follow us</h6>
+              <NavLink to="#!" className="btn btn-primary btn-floating m-1">
+                <i className="fab fa-facebook-f"></i>
+              </NavLink>
+              <NavLink to="#!" className="btn btn-primary btn-floating m-1">
+                <i className="fab fa-twitter"></i>
+              </NavLink>
+              <NavLink to="#!" className="btn btn-primary btn-floating m-1">
+                <i className="fab fa-google"></i>
+              </NavLink>
+              <NavLink to="#!" className="btn btn-primary btn-floating m-1">
+                <i className="fab fa-instagram"></i>
+              </NavLink>
+              <NavLink to="#!" className="btn btn-primary btn-floating m-1">
+                <i className="fab fa-linkedin-in"></i>
+              </NavLink>
+              <NavLink to="#!" className="btn btn-primary btn-floating m-1">
+                <i className="fab fa-github"></i>
+              </NavLink>
+            </div>
+          </div>
+        </section>
+      </div>
+      <div className="text-center p-3">
+        © 2020 Copyright:
+        <NavLink to="https://mdbootstrap.com/">Actividad 1</NavLink>
+        MDBootstrap.com
+      </div>
     </footer>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,22 +4,74 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <header>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/miproyecto" activeClassName="active" exact>
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/actividad1" activeClassName="active">
-              Actividad 1
-            </NavLink>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <NavLink to="/miproyecto" className="navbar-brand">
+            NavBar
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink to="/miproyecto" className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}>
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/actividad1" className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}>
+                  Actividad 1
+                </NavLink>
+              </li>
+              <li className="nav-item dropdown">
+                <NavLink
+                  to="#"
+                  className="nav-link dropdown-toggle"
+                  id="navbarDropdown"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </NavLink>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li className="nav-item">
+                    <NavLink to="/actividad1" className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}>
+                      Actividad 1
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink to="/actividad1" className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}>
+                      Actividad 1
+                    </NavLink>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li className="nav-item">
+                    <NavLink to="/actividad1" className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}>
+                      Actividad 1
+                    </NavLink>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
       </nav>
     </header>
   );
